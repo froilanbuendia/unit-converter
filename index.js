98a4a73f800efa9282cbe3ce4a9417837fc38b77
froilanbuendia <froilangbuendia@gmail.com>
2025-06-24 22:54:17 -0700

    requirements: light mode

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const headerButton = document.getElementById("header-button");
+const input = document.getElementById("header-input");
+const meterEl = document.getElementById("meter-result");
+const literEl = document.getElementById("liter-result");
+const kilogramEl = document.getElementById("kilogram-result");
+
+headerButton.addEventListener("click", () => {
+  renderResult();
+});
+
+const renderResult = () => {
+  const inputValue = input.value;
+  const meterResult = (inputValue * 3.28084).toFixed(3);
+  const feetResult = (inputValue / 3.28084).toFixed(3);
+  const literResult = (inputValue * 0.264172).toFixed(3);
+  const gallonResult = (inputValue / 0.264172).toFixed(3);
+  const kilogramResult = (inputValue * 2.20462).toFixed(3);
+  const poundResult = (inputValue / 2.20462).toFixed(3);
+
+  meterEl.innerHTML = `${inputValue} meters = ${meterResult} feet | ${inputValue} feet = ${feetResult} meters`;
+  literEl.innerHTML = `${inputValue} liters = ${literResult} gallons | ${inputValue} gallons = ${gallonResult} liters`;
+  kilogramEl.innerHTML = `${inputValue} kilos = ${kilogramResult} pounds | ${inputValue} pounds = ${poundResult} kilos`;
+  meterEl.innerHTML = `${inputValue} meters = ${meterResult} feet | ${inputValue} feet = ${feetResult} meters`;
+  literEl.innerHTML = `${inputValue} liters = ${literResult} gallons | ${inputValue} gallons = ${gallonResult} liters`;
+  kilogramEl.innerHTML = `${inputValue} kilos = ${kilogramResult} pounds | ${inputValue} pounds = ${poundResult} kilos`;
+};
+
+renderResult();
